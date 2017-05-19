4856e445a4eb99029025f4531940032600712297
Adam Ralph <adam@adamralph.com>
2017-05-19 09:10:28 +0300

    prepare the scene for no magic

---
+++ NetCoreApp20/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NetCoreApp20
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Abracadabra...");
+
+            //new NetStdClass().Run()
+            //    .GetAwaiter().GetResult();
+
+            Console.WriteLine("Hey presto!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NetStd20/NetStdClass.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+
+namespace NetStd20
+{
+    public class NetStdClass
+    {
+        public async Task Run()
+        {
+            //var config = new EndpointConfiguration("nomagic");
+            //config.UseTransport<MsmqTransport>();
+            //config.UsePersistence<InMemoryPersistence>();
+            //config.AuditProcessedMessagesTo("audit");
+            //config.SendFailedMessagesTo("error");
+            //config.EnableInstallers();
+
+            //var endpointInstance = await Endpoint.Start(config)
+            //    .ConfigureAwait(false);
+
+            //try
+            //{
+            //    Console.WriteLine("Press any key to stop.");
+            //    Console.ReadKey();
+            //}
+            //finally
+            //{
+            //    await endpointInstance.Stop()
+            //        .ConfigureAwait(false);
+
+            //}
+        }
+    }
+}
